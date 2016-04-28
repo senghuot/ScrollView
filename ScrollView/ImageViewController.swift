a702d6b2a8545bf42d788a93162e2ba65d419436
senghuot <senghuotlim@gmail.com>
2016-04-27 21:56:25 -0700

    rewrote and still works :)

--- ScrollView/ImageViewController.swift
+++ ScrollView/ImageViewController.swift
@@ -10,47 +10,102 @@ import UIKit
 
 class ImageViewController: UIViewController, UIScrollViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        scrollView.addSubview(imageView)
-        if image == nil {
-            fetchImage()
-        }
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        scrollView.addSubview(imageView)
+//        if image == nil {
+//            fetchImage()
+//        }
+//    }
+//    
+//    
+//    override func viewWillAppear(animated: Bool) {
+//        super.viewWillAppear(animated)
+//        if image == nil {
+//            fetchImage()
+//        }
+//    }
+//    
+//    var imageURL: NSURL? {
+//        didSet {
+//            image = nil
+//            if view.window != nil {
+//                fetchImage()
+//            }
+//        }
+//    }
+//    
+//    private func fetchImage() {
+//        if let url = imageURL {
+//            print(url)
+//            let imageData = NSData(contentsOfURL: url)
+//            if imageData == nil {
+//                print("image data is nil")
+//                image = nil
+//            } else {
+//                image = UIImage(data: imageData!)
+//            }
+//        }
+//    }
+//    
+//    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+//        return imageView
+//    }
+//    
+//    @IBOutlet weak var scrollView: UIScrollView! {
+//        didSet {
+//            scrollView.contentSize = imageView.frame.size
+//            scrollView.delegate = self
+//            scrollView.minimumZoomScale = 0.01
+//            scrollView.maximumZoomScale = 1.0
+//        }
+//    }
+//    
+//    
+//    private var imageView = UIImageView()
+//    private var image: UIImage? {
+//        get {
+//            return imageView.image
+//        }
+//        set {
+//            imageView.image = newValue
+//            imageView.sizeToFit()
+//            scrollView?.contentSize = imageView.frame.size
+//        }
+//    }
     
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        if image == nil {
-            fetchImage()
-        }
-    }
     
     var imageURL: NSURL? {
         didSet {
             image = nil
-            if view.window != nil {
-                fetchImage()
-            }
+            fetchImage()
         }
     }
     
-    private func fetchImage() {
+    func fetchImage() {
         if let url = imageURL {
-            print(url)
             let imageData = NSData(contentsOfURL: url)
-            if imageData == nil {
-                print("image data is nil")
-                image = nil
-            } else {
+            if imageData != nil {
                 image = UIImage(data: imageData!)
+            } else {
+                image = nil
             }
         }
     }
     
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return imageView
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+        
+        get {
+            return imageView.image
+        }
     }
     
     @IBOutlet weak var scrollView: UIScrollView! {
@@ -62,18 +117,19 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    
-    private var imageView = UIImageView()
-    private var image: UIImage? {
-        get {
-            return imageView.image
-        }
-        set {
-            imageView.image = newValue
-            imageView.sizeToFit()
-            scrollView?.contentSize = imageView.frame.size
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+        if image == nil {
+            fetchImage()
         }
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
 
 }
--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -15,19 +15,21 @@ class ViewController: UIViewController {
             if let identifier = segue.identifier {
                 switch identifier {
                 case "Earth":
-                    ivc.imageURL = DemoURL.NASA.Earth
                     ivc.title = "Earth"
-                case "Saturn":
-                    ivc.imageURL = DemoURL.NASA.Saturn
-                    ivc.title = "Saturn"
+                    ivc.imageURL = DemoURL.NASA.Earth
                 case "Cassini":
-                    ivc.imageURL = DemoURL.NASA.Cassini
                     ivc.title = "Cassini"
+                    ivc.imageURL = DemoURL.NASA.Cassini
+                case "Saturn":
+                    ivc.title = "Saturn"
+                    ivc.imageURL = DemoURL.NASA.Saturn
                 default: break
                 }
+                
             }
+            
         }
+        
     }
-
 }
 
